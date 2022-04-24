bf6b690dc09620f5032e46753f14ebaab8776cd6
DhanashriNerkar <dhanashrinerkar2@gmail.com>
2022-04-24 23:24:02 +0530

    UC1 and UC2 added

---
+++ Employee_wage.java
@@ -0,0 +1,26 @@
+package Day4_Assignment;
+import java.util.Random;
+
+
+public class Employee_wage 
+{
+
+	public static final int part_time=1;
+	public static final int full_time=2;
+	public static final int empwageperhr=20;
+
+	int emphrs=0;
+	int empwage=0;
+	Random r=new Random();
+	int empcheck=r.nextInt(3);
+
+		public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
+
+
